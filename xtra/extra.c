b12744288fa85f4709258e7d73cb49e409d9fcd4
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-03-26 19:53:57 -0300

    problem 1

--- xtra/extra.c
+++ xtra/extra.c
@@ -35,136 +35,112 @@ void xtra(FILE *file) {
 
 
     while (fread(instruction, sizeof(char), 2, file) >= 2) {
-        if(instruction[0] == 0x00 && instruction[1] == 0x00){
-            break;
-        }
-
-        int no_of_operands, bit, reg, source, dest;
-        no_of_operands = (instruction[0] >> 6) & 3;
+        char word[2];
+        int source, dest,val;
 
         if (flag) {
             printf("    call debug\n");
         }
-        switch (no_of_operands) {
-            case 0:
-                switch ((int)instruction[0]) {
-                    case I_RET:
-                        break;
-                    case I_CLD:
-                        flag = 0;
-                        break;
-                    case I_STD:
-                        flag = 1;
-                        break;
-                }
-                break;
-            case 1:
-
-                bit = (instruction[0] >> 5) & 1;
-                if (bit) {
-                    switch ((int)instruction[0]) {
-                        case I_BR:
-                            break;
-                        case I_JR:
-                            break;
-                    }
-                } else {
-                    reg = (instruction[1] >> 4) & 0xf;
-                    switch ((int)instruction[0]) {
-                        case I_NEG:
-                            printf("neg %s\n", reg_map64[reg]);
-                            break;
-                        case I_NOT:
-                            printf("neg %s\n", reg_map64[reg]);
-                            break;
-                        case I_INC:
-                            printf("inc %s\n", reg_map64[reg]);
-                            break;
-                        case I_DEC:
-                            printf("dec %s\n", reg_map64[reg]);
-                            break;
-                        case I_PUSH:
-                            printf("push %s\n", reg_map64[reg]);
-                            break;
-                        case I_POP:
-                            printf("pop %s\n", reg_map64[reg]);
-                            break;
-                        case I_OUT:
-                            break;
-                    }
-                }
-                break;
-            case 2:
-                source = (instruction[1] >> 4) & 0xf;
-                dest = (instruction[1] & 0x0f);
-
-                switch ((int)instruction[0]) {
-                    case I_ADD:
-                        printf("add %s, %s\n", reg_map64[source], reg_map64[dest]);
-                        break;
-                    case I_SUB:
-                        printf("sub %s, %s\n", reg_map64[source], reg_map64[dest]);
-                        break;
-                    case I_MUL:
-                        printf("imul %s, %s\n", reg_map64[source], reg_map64[dest]);
-                        break;
-                    case I_AND:
-                        printf("add %s, %s\n", reg_map64[source], reg_map64[dest]);
-                        break;
-                    case I_OR:
-                        printf("or %s, %s\n",reg_map64[source],reg_map64[dest]);
-                        break;
-                    case I_XOR:
-                        printf("xor %s, %s\n",reg_map64[source],reg_map64[dest]);
-                        break;
-                    case I_TEST:
-                        if((source & dest) != 0 ){
-                            printf("mov $0x1, %s\n",reg_map64[13]);
-                        }else{
-                            printf("mov $0x0, %s\n",reg_map64[13]);
-                        }
-                        break;
-                    case I_CMP:
-                        if(source < dest){
-                            printf("mov $0x1, %s\n",reg_map64[13]);
-                        }else{
-                            printf("mov $0x0, %s\n",reg_map64[13]);
-                        }
-                        break;
-                    case I_EQU:
-                        if(source == dest){
-                            printf("mov $0x1, %s\n",reg_map64[13]);
-                        }else{
-                            printf("mov $0x0, %s\n",reg_map64[13]);
-                        }
-                        break;
-                    case I_MOV:
-                        printf("mov %s, %s\n",reg_map64[source],reg_map64[dest]);
-                        break;
-                    case I_LOAD:
-                        break;
-                    case I_STOR:
-                        break;
-                    case I_LOADB:
-                        break;
-                    case I_STORB:
-                        break;
-                }
-                break;
-            case 3:
-                switch ((int)instruction[0]) {
-                    case I_JMP:
-                        break;
-                    case I_CALL:
-                        break;
-                    case I_LOADI:
-                        break;
-                }
-
-                break;
-            default:
-                break;
+        if (instruction[0] == 0x00 && instruction[1] == 0x00) {
+            break;
         }
+        if (((instruction[0] >> 6) & 0x03) == XIS_EXTENDED) {
+            fread(word, sizeof(char),2,file);
+        }
+
+        source = (instruction[1] >> 4) & 0xf;
+        dest = instruction[1] & 0x0f;
+
+        if (flag) {
+            printf("    call debug\n");
+        }
+        switch ((int) instruction[0]) {
+            case I_RET:
+                break;
+            case I_CLD:
+                flag = 0;
+                break;
+            case I_STD:
+                flag = 1;
+                break;
+            case I_BR:
+                break;
+            case I_JR:
+                break;
+            case I_NEG:
+                printf("    neg %s\n", reg_map64[source]);
+                break;
+            case I_NOT:
+                printf("    not %s\n", reg_map64[source]);
+                break;
+            case I_INC:
+                printf("    inc %s\n", reg_map64[source]);
+                break;
+            case I_DEC:
+                printf("    dec %s\n", reg_map64[source]);
+                break;
+            case I_PUSH:
+                printf("    push %s\n", reg_map64[source]);
+                break;
+            case I_POP:
+                printf("    pop %s\n", reg_map64[source]);
+                break;
+            case I_OUT:
+                break;
+            case I_ADD:
+                printf("    add %s, %s\n", reg_map64[source], reg_map64[dest]);
+                break;
+            case I_SUB:
+                printf("    sub %s, %s\n", reg_map64[source], reg_map64[dest]);
+                break;
+            case I_MUL:
+                printf("    imul %s, %s\n", reg_map64[source], reg_map64[dest]);
+                break;
+            case I_AND:
+                printf("    add %s, %s\n", reg_map64[source], reg_map64[dest]);
+                break;
+            case I_OR:
+                printf("    or %s, %s\n", reg_map64[source], reg_map64[dest]);
+                break;
+            case I_XOR:
+                printf("    xor %s, %s\n", reg_map64[source], reg_map64[dest]);
+                break;
+            case I_TEST:
+                printf("    test %s, %s\n", reg_map64[source], reg_map64[dest]);
+                printf("    setnz %s\n", reg_map64[13]);
+                break;
+            case I_CMP:
+                printf("    cmp %s, %s\n", reg_map64[source], reg_map64[dest]);
+                printf("    setg %s\n", reg_map64[13]);
+                break;
+            case I_EQU:
+                printf("    cmp %s, %s\n", reg_map64[source], reg_map64[dest]);
+                printf("    sete %s\n", reg_map64[13]);
+                break;
+            case I_MOV:
+                printf("    mov %s, %s\n", reg_map64[source], reg_map64[dest]);
+                break;
+            case I_LOAD:
+                break;
+            case I_STOR:
+                break;
+            case I_LOADB:
+                break;
+            case I_STORB:
+                break;
+            case I_JMP:
+                printf("    jmp %s", word);
+                break;
+            case I_CALL:
+                printf("    call %s", word);
+                break;
+            case I_LOADI:
+                printf("    mov $%s, %s",word,reg_map64[source]);
+                break;
+
+
+        }
+
     }
 
 }
