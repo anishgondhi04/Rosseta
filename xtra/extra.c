45520b6a53a856eb6a866ec327396289c985390f
Anish Gondhi <159081051+AnishGondhiDC@users.noreply.github.com>
2024-03-26 21:45:20 -0300

    problem2 and commited

--- xtra/extra.c
+++ xtra/extra.c
@@ -9,6 +9,7 @@
 
 void xtra(FILE *file) {
 
+    //this is register mapping
     char *reg_map64[] = {
             "%rax",
             "%rbx",
@@ -28,28 +29,47 @@ void xtra(FILE *file) {
             "%rsp",  // r15  stack pointer
             "%rip"
     };
+    char *reg_map8[] = {
+            "%al",
+            "%bl",
+            "%cl",
+            "%dl"
+    };
 
-
+    //instruction array is used to read instruction in binary from the given file
+    //flag takes care of the debug function call
+    //offset is used to set the labels before each instruction
     char instruction[2];
     int flag = 0;
     long int offset = 0x0000;
 
+    //This loop runs until the there is no bits left in the file.
     while (fread(instruction, sizeof(char), 2, file) >= 2) {
+
+        //word is used to read word for extended instructions
+        //source is the first 4 bits in second byte which encodes source register
+        //dest is the last 4 bits in second byte which encodes dest register
         char word[2];
         int source, dest,inst_f,inst_l,val;
 
+        //filtering the encoding storing first and last byte separately
         inst_f = instruction[0] & 0xff;
         inst_l = instruction[1] & 0xff;
 
+        //printing the label
         printf(".L%04x:\n",offset);
         offset += 2;
 
+        //checking if debug is set
         if (flag) {
             printf("    call debug\n");
         }
+        //checking if first and last byte are 0
         if (inst_f == 0x00 && inst_l == 0x00) {
             break;
         }
+
+        //checking if extended
         if (((inst_f >> 6) & 0x03) == XIS_EXTENDED) {
             fread(word, sizeof(char),2,file);
             val = word[0] & 0xff;
@@ -61,6 +81,8 @@ void xtra(FILE *file) {
         source = (inst_l >> 4) & 0xf;
         dest = inst_l & 0x0f;
 
+
+        //switch covers all the encoding for 30 instruction in x
         switch (inst_f) {
             case I_RET:
                 printf("    ret\n");
@@ -133,16 +155,16 @@ void xtra(FILE *file) {
                 printf("    mov %s, %s\n", reg_map64[source], reg_map64[dest]);
                 break;
             case I_LOAD:
-                printf("    mov (%s), %s",reg_map64[source],reg_map64[dest]);
+                printf("    mov (%s), %s\n",reg_map64[source],reg_map64[dest]);
                 break;
             case I_STOR:
-                printf("    mov %s, (%s)",reg_map64[source],reg_map64[dest]);
+                printf("    mov %s, (%s)\n",reg_map64[source],reg_map64[dest]);
                 break;
             case I_LOADB:
-                printf("    movb (%s), %s",reg_map64[source],reg_map64[dest]);
+                printf("    movb (%s), %s\n",reg_map64[source],reg_map8[dest]);
                 break;
             case I_STORB:
-                printf("    movb %s, (%s)",reg_map64[source],reg_map64[dest]);
+                printf("    movb %s, (%s)\n",reg_map8[source],reg_map64[dest]);
                 break;
             case I_JMP:
                 printf("    jmp .L%04x\n", val);
--- xtra/main.c
+++ xtra/main.c
@@ -3,23 +3,31 @@
 #include "extra.h"
 
 int main(int argc, char **argv) {
+
+    //checking if file is passed as an arguement
     if (argc <= 1) {
         printf("Please input the file");
         return 1;
     }
 
+    //file pointer to binary file
     FILE *file = fopen(argv[1],"rb");
     assert(file);
 
+    //printing the prologue
     printf(".globl test\n"
            "test:\n"
            "    push %%rbp\n"
            "    mov %%rsp, %%rbp\n");
+
+    //calling xtra to translate the x to x86
     xtra(file);
 
+    //printing the epilogue
     printf("    pop %%rbp\n"
            "    ret\n");
 
+    //closing file
     fclose(file);
 
     return 0;
